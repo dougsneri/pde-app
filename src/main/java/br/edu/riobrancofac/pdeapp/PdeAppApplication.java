f92d40b4de0dbe5825f83a3b4f6c154cbc1edb8d
dougsneri <dsds.douglas@gmail.com>
2019-08-16 17:05:51 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/riobrancofac/pdeapp/PdeAppApplication.java
@@ -0,0 +1,13 @@
+package br.edu.riobrancofac.pdeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdeAppApplication.class, args);
+	}
+
+}
